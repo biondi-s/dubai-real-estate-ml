a6c54785e94ad8ac6e865c4a9f93acc4bf381575
biondi-s <biondi.s@outlook.com>
2025-12-30 15:07:09 +0100

    initial commit

---
+++ utils.py
@@ -0,0 +1,38 @@
+import googlemaps
+
+GOOGLE_MAPS_API_KEY = "YOUR_API_KEY_HERE"
+
+
+def geocode_with_quality(address: str):
+    try:
+        gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
+        results = gmaps.geocode(address)
+        if not results:
+            return {
+                "lat": None,
+                "lng": None,
+                "location_type": None,
+                "partial_match": None,
+                "formatted_address": None,
+            }
+
+        r0 = results[0]
+        loc = r0["geometry"]["location"]
+
+        return {
+            "lat": loc["lat"],
+            "lng": loc["lng"],
+            "location_type": r0["geometry"].get("location_type"),
+            "partial_match": r0.get("partial_match", False),
+            "formatted_address": r0.get("formatted_address"),
+        }
+
+    except Exception as e:
+        # optional: log e
+        return {
+            "lat": None,
+            "lng": None,
+            "location_type": None,
+            "partial_match": None,
+            "formatted_address": None,
+        }
